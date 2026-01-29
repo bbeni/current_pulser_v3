851654afef34c01a1ac43a28ec12ff2040e219c3
Benjamin Froelich <bbenif@gmail.com>
2026-01-29 12:25:29 +0100

    continuous measurement 'shift screen' 2

--- src/main_osci.c
+++ src/main_osci.c
@@ -132,16 +132,19 @@ void osc_shift_screen_setup(struct Osc_Device* device, double** data_out, int* n
     memset(data, 0x0, sizeof(*data) * *n_samples_out);
 
     *data_out = data;
+
+    // start
+    FDwfAnalogInConfigure(device->handle, 0, true);
 }
 
 void osc_shift_screen_update(struct Osc_Device* device, double* data_out, int n_samples) {
 
-    // start
-    FDwfAnalogInConfigure(device->handle, 0, true);
+    if (FDwfAnalogInStatus(device->handle, true, &device->status) != stsDone) return;
 
-    do {
-        FDwfAnalogInStatus(device->handle, true, &device->status);
-    } while (device->status != stsDone);
+    int n_valid_samples;
+    FDwfDigitalInStatusSamplesValid(device->handle, &n_valid_samples);
+
+    printf("number of valid samples: %d\n", n_valid_samples);
 
     // get the samples for each channel
     for (int c = 0; c < device->n_channels; c++) {
@@ -151,6 +154,7 @@ void osc_shift_screen_update(struct Osc_Device* device, double* data_out, int n_
     // scan bar
     int bar_index;
     FDwfDigitalInStatusIndexWrite(device->handle, &bar_index);
+    printf("scan bar index: %d\n", bar_index);
 
 }
 
