f706699d295670d54e99d6d052856018113c95ac
Benjamin Froelich <bbenif@gmail.com>
2026-04-09 21:28:46 +0200

    hv supply test 4

--- src/config.h
+++ src/config.h
@@ -11,7 +11,7 @@ const float CONFIG_REARM_COOLDOWN_SECONDS = 90.0f;
 const float CONFIG_MAX_VOLTAGE = 2000.0f;
 const float CONFIG_MIN_VOLTAGE = 0.0f;
 
-const float CONFIG_TARGET_CHARGING_CURRENT = 300.0f; // milli ampere
+const float CONFIG_TARGET_CHARGING_CURRENT = 321.0f; // Ampere
 
 
 #endif // CONFIG_H_
