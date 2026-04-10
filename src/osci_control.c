00211b35001e077d2b7f9720760738a565692dc1
Benjamin Froelich <bbenif@gmail.com>
2026-04-10 19:47:54 +0200

    display labels and csv filename 3

--- src/osci_control.c
+++ src/osci_control.c
@@ -437,7 +437,9 @@ void oscilloscope_ui_draw(Mui_Rectangle area, float grid_pixel_unit, struct Osci
         Mui_Vector2 measure = mui_measure_text(filename, 0, l, mui_protos_theme_g.font_label, mui_protos_theme_g.font_label_size, 0.5f);
         pos.x -= measure.x * 0.5f;
         pos.y -= measure.y * 0.5f;
-        mui_draw_text_line(filename, 0, l, mui_protos_theme_g.font_label, mui_protos_theme_g.font_label_size, 0.5f, MUI_YELLOW, pos);
+        pos.x = plot_rect.x;
+        pos.y = plot_rect.y;
+        mui_draw_text_line(filename, 0, l, mui_protos_theme_g.font_label, mui_protos_theme_g.font_label_size, 0.5f, MUI_GRAY, pos);
     }
 
     // legend
